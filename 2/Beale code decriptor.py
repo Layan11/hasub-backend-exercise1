b968cea5eb4bd27cb809e11813c2108b0361035b
Layan11 <layan.haddad0@gmail.com>
2024-02-19 16:20:38 +0200

    second

--- 2/Beale code decriptor.py
+++ 2/Beale code decriptor.py
@@ -1,5 +1,5 @@
 
-def gen_even_arr(values, keys):
+def gen_keys_arr(values, keys):
     for k in keys:
         if values[k]:
             yield values[k][0]
@@ -12,13 +12,11 @@ if __name__ == '__main__':
     keys = [4, 93, 52, 12, 41, 23, 9, 1, 34, 2, 11, 111, 6, 13, 24, 99, 100,
             30, 10, 26, 16, 29, 155, 32, 37, 61, 15, 42, 3, 633, 27, 70, 77,
             45, 55, 43, 35, 108, 103, 56, 159, 166, 7, 8, 174, 36]
-    # print(declaration_of_independence)
 
-
-    my_gen_arr = gen_even_arr(declaration_of_independence, keys)
-    arr = []
+    my_gen_arr = gen_keys_arr(declaration_of_independence, keys)
+    words = []
     for i in my_gen_arr:
-        arr.append(i)
-    print(arr)
+        words.append(i)
+    print(words)
 
 #our plan is to get all of apples stock as soon as possible!
