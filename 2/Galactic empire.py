ce638967e601b823564a6658e4007d96802a282b
yishain11 <yishain11@walla.co.il>
2024-02-25 13:26:36 +0200

    Update Galactic empire.py

--- 2/Galactic empire.py
+++ 2/Galactic empire.py
@@ -7,16 +7,18 @@ class AlienDelegation:
         self.suggestions = suggestions
         self.wanted_demands = wanted_demands
 
-
+### why add this line?
 if __name__ == '__main__':
-    materials = [i for i in range(21)]
+    materials = [i for i in range(21)] ### good for placeholder, where are the materials?
+    ### DRY
     AD1 = AlienDelegation("AD1", 1, [14, 4])
     AD2 = AlienDelegation("AD2", 11, [1])
     AD3 = AlienDelegation("AD3", 2, [15,3,4])
     AD4 = AlienDelegation("AD4", 7, [2,3,4,5,6,7,8])
+    ### what if it was 500 delegations?
     AD = [AD1, AD2, AD3, AD4]
     convinced_aliens = []
-    for ad in AD:
+    for ad in AD: ### is this name clear? what is ad? what is AD? think you read it after a month.
         accepted = False
         while not accepted and ad.suggestions > 0:
             given_material = randint(1, 21)
